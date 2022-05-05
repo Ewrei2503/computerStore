69954949cf72d550f7e8aa4ea8ea88f0f551dbd5
Ewrei2503 <egor.kulesh.02@mail.ru>
2022-05-05 21:50:50 +0300

    Removed Mustache, added Freemarker. Added DB Connections

--- src/main/java/com/example/computerStock/domain/Role.java
+++ src/main/java/com/example/computerStock/domain/Role.java
@@ -1,7 +1,12 @@
 package com.example.computerStock.domain;
 
-public enum Role {
-    USER,
-    ADMIN,
-    ZAVSKLAD;
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
